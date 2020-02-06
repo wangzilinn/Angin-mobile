3ea00f63667cf829ee85e91fd5241ca5269faee0
Wangzilin <wangzilinn@gmail.com>
2020-02-06 19:03:33 +0800

    大概弄明白了图表怎么用

--- lib/views/card/card_statistics_view.dart
+++ lib/views/card/card_statistics_view.dart
@@ -1,5 +1,15 @@
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
-import 'package:flutter_echarts/flutter_echarts.dart';
+
+class ClicksPerYear {
+  final String year;
+  final int clicks;
+  final charts.Color color;
+
+  ClicksPerYear(this.year, this.clicks, Color color)
+      : this.color = new charts.Color(
+            r: color.red, g: color.green, b: color.blue, a: color.alpha);
+}
 
 class CardStatisticsView extends StatefulWidget {
   @override
@@ -9,6 +19,59 @@ class CardStatisticsView extends StatefulWidget {
 class _CardStatisticsViewState extends State<CardStatisticsView> {
   @override
   Widget build(BuildContext context) {
+    var data = [
+      new ClicksPerYear('2016', 12, Colors.red),
+      new ClicksPerYear('2017', 42, Colors.yellow),
+      new ClicksPerYear('2018', 30, Colors.green),
+    ];
+
+    var series = [
+      new charts.Series(
+        id: 'Clicks',
+        domainFn: (ClicksPerYear clickData, _) => clickData.year,
+        measureFn: (ClicksPerYear clickData, _) => clickData.clicks,
+        colorFn: (ClicksPerYear clickData, _) => clickData.color,
+        data: data,
+      ),
+    ];
+
+    var series2 = [
+      new charts.Series(
+        id: 'Clicks',
+        domainFn: (ClicksPerYear clickData, _) => int.parse(clickData.year),
+        measureFn: (ClicksPerYear clickData, _) => clickData.clicks,
+        colorFn: (ClicksPerYear clickData, _) => clickData.color,
+        data: data,
+      ),
+    ];
+
+    var chart1 = new charts.BarChart(
+      series,
+      animate: true,
+    );
+    var chart2 = new charts.LineChart(
+      series2,
+      animate: true,
+      domainAxis: charts.NumericAxisSpec( // 主轴的配置
+//        tickFormatterSpec: DomainFormatterSpec(widget.dateRange), // tick 值的格式化，这里把 num 转换成 String
+        renderSpec: charts.SmallTickRendererSpec( // 主轴绘制的配置
+          tickLengthPx: 0, // 刻度标识突出的长度
+          labelOffsetFromAxisPx: 12, // 刻度文字距离轴线的位移
+//          labelStyle: charts.TextStyleSpec( // 刻度文字的样式
+//            color: ChartUtil.getChartColor(HColors.lightGrey),
+//            fontSize: HFontSizes.smaller.toInt(),
+//          ),
+//          axisLineStyle: charts.LineStyleSpec( // 轴线的样式
+//            color: ChartUtil.getChartColor(ChartUtil.lightBlue),
+//          ),
+        ),
+        tickProviderSpec: charts.BasicNumericTickProviderSpec( // 轴线刻度配置
+          zeroBound:false,
+          dataIsInWholeNumbers: false,
+          desiredTickCount: 3, // 期望显示几个刻度
+        ),
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text("Card statistics"),
@@ -19,27 +82,14 @@ class _CardStatisticsViewState extends State<CardStatisticsView> {
           },
         ),
       ),
-      body: Container(
-        color: Colors.red,
-        child: Echarts(
-          option: '''
-    {
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [{
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line'
-      }]
-    }
-  ''',
-        ),
-        width: 300,
-        height: 250,
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 5, child: chart1),
+          Expanded(
+            flex: 5,
+            child: chart2,
+          )
+        ],
       ),
     );
   }
